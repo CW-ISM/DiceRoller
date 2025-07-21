245be094accf6b14412ce6caf5077d517786c9a0
CW-ISM <cwismgitism@gmail.com>
2025-07-21 01:11:21 -0700

    Add Die class for dice simulation in DiceRoller
    Introduces a new 'Die' class that models a die with configurable number of sides. The constructor enforces a constraint on the number of sides, allowing values between 1 and 20. A property 'NumberOfSides' is provided to access the number of sides of the die.

---
+++ DiceRoller/Die.cs
@@ -0,0 +1,35 @@
+﻿namespace DiceRoller;
+public class Die
+{
+    /// <summary>
+    /// Represents a die used in games or simulations
+    /// with a configurable nubmer of sides.
+    /// </summary>
+    /// <param name="numSides">
+    /// The number of sides for the die. Must be greater than zero
+    /// </param>
+    
+    /// <summary>
+    /// Gets the die's number of sides.
+    /// </summary>
+    public byte NumSides { get; init; }
+    private Random random;
+
+    public Die(byte numSides)
+    {
+        if(numSides < 0)
+        {
+            throw new ArgumentOutOfRangeException(
+                    nameof(numSides),
+                    $"{nameof(numSides)} must be greater than 0"
+                );
+        }
+        NumSides = numSides;
+        random = new Random();
+    }
+
+    public byte Roll()
+    {
+        return (byte)(((byte)random.Next()) % this.NumSides);
+    }
+}
