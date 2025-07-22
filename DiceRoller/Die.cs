235e6e86513d4860e2d36e2aae0d90b8b16eada5
CW-ISM <cwismgitism@gmail.com>
2025-07-21 17:42:14 -0700

    Added two constants MAX_SIDES and MIN_SIDES to get rid of magic numbers in the Die constructor.

--- DiceRoller/Die.cs
+++ DiceRoller/Die.cs
@@ -2,29 +2,6 @@
 public class Die
 {
     /// <summary>
-    /// Represents a die used in games or simulations
-    /// with a configurable nubmer of sides.
-    /// </summary>
-    /// <param name="numSides">
-    /// The number of sides for the die. Must be greater than zero
-    /// </param>
-    /// <exception cref="ArgumentOutOfRangeException">Thrown when invalid number of sides is provided</exception>
-    public Die(byte numSides)
-    {
-        if(numSides == 0 || numSides > 20)
-        {
-            throw new ArgumentOutOfRangeException(
-                    nameof(numSides),
-                    $"{nameof(numSides)} must be greater than 0 and less than 21"
-                );
-        }
-        NumSides = numSides;
-
-        // Force an initial roll
-        Roll();
-    }
-
-    /// <summary>
     /// Gets the die's number of sides.
     /// </summary>
     public byte NumSides { get; init; }
@@ -38,6 +15,33 @@ public class Die
     /// A private random number generator used to simulate die rolls.
     /// </summary>
     private readonly static Random random = new();
+
+    private const byte MAX_SIDES = 20;
+    private const byte MIN_SIDES = 2;
+
+
+    /// <summary>
+    /// Represents a die used in games or simulations
+    /// with a configurable nubmer of sides.
+    /// </summary>
+    /// <param name="numSides">
+    /// The number of sides for the die. Must be greater than zero
+    /// </param>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when invalid number of sides is provided</exception>
+    public Die(byte numSides)
+    {
+        if(numSides < MIN_SIDES || numSides > MAX_SIDES)
+        {
+            throw new ArgumentOutOfRangeException(
+                    nameof(numSides),
+                    $"{nameof(numSides)} must be greater than 0 and less than 21"
+                );
+        }
+        NumSides = numSides;
+
+        // Force an initial roll
+        Roll();
+    }
 
     public byte Roll()
     {
